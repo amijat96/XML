c8f68849523f8852fe96360cfe16b0cf44f991d5
Aleksandar <Aleksandar@DESKTOP-HGB8QLO>
2019-09-03 18:38:13 +0200

    DTO p1

---
+++ XMLWS-i-Bezbednost/src/main/java/ftn/XMLWSiBezbednost/dto/AbstractEntityDTO.java
@@ -0,0 +1,27 @@
+package ftn.XMLWSiBezbednost.dto;
+
+public abstract class AbstractEntityDTO {
+
+    protected int id;
+    protected boolean deleted;
+
+    public AbstractEntityDTO() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+
+}
---
+++ XMLWS-i-Bezbednost/src/main/java/ftn/XMLWSiBezbednost/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package ftn.XMLWSiBezbednost.dto;
+
+import java.util.Date;
+
+
+
+public class UserDTO extends AbstractEntityDTO {
+
+    protected String email;
+    protected String password;
+    protected Date signupDate;
+    protected boolean isAdmin = false;
+
+    public UserDTO() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getSignupDate() {
+        return signupDate;
+    }
+
+    public void setSignupDate(Date signupDate) {
+        this.signupDate = signupDate;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
