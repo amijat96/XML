60cae4993dc2b12f3117b7720bb3a359ce1a29cb
Aleksandar <Aleksandar@DESKTOP-HGB8QLO>
2019-09-03 21:53:32 +0200

    dorada cas-a

--- XMLWS-i-Bezbednost/src/main/java/ftn/XMLWSiBezbednost/service/CAService.java
+++ XMLWS-i-Bezbednost/src/main/java/ftn/XMLWSiBezbednost/service/CAService.java
@@ -1,8 +1,12 @@
 package ftn.XMLWSiBezbednost.service;
 
+import java.util.List;
+
 import ftn.XMLWSiBezbednost.dto.CertificateAuthorityDTO;
+import ftn.XMLWSiBezbednost.exception.ActionNotPossibleException;
 import ftn.XMLWSiBezbednost.exception.EntityNotFoundException;
 import ftn.XMLWSiBezbednost.utils.data.CertificateAuthority;
+import ftn.XMLWSiBezbednost.utils.data.CertificateData;
 
 public interface CAService {
 
@@ -15,4 +19,16 @@ public interface CAService {
 	    void generateHTTPSCertificate();
 
 	    CertificateAuthority findById(int id) throws EntityNotFoundException;
+	    
+	    
+	    List<CertificateAuthority> getByRole(CertificateAuthority.CARole role);
+
+	    List<CertificateAuthority> getBottomCAs();
+
+	    List<CertificateAuthority> getAllCAs();
+
+	    CertificateAuthority getRandomCAForUsage(CertificateData.CertUsage usage) throws ActionNotPossibleException;
+
+	    CertificateAuthority save(CertificateAuthority ca);
 }
+
